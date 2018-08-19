85fc299c75d182ad7d76bdf624f008b711758868
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-19 01:00:05 -0300

    Implementando diretivas
    Implementando as diretivas do angular no html

--- src/app/tarefas/editar/editar-tarefa.component.ts
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { TarefaService, Tarefa } from './../shared';
 
 @Component({
   selector: 'app-editar-tarefa',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa') formTarefa :NgForm;
+  tarefa: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.tarefa = this.tarefaService.buscarPorId(id);
   }
 
+  atualizar(): void{
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.atualizar(this.tarefa);
+      this.router.navigate(['/tarefas']);
+    }
+  }
 }
